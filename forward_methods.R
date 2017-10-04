8d9cd294ecda78ad5d69d7736d4902cac7d39c8c
Nathan Wycoff <nathanbrwycoff@gmail.com>
2017-10-03 21:41:45 -0400

    Cleaned up; forward SMACOF works

--- forward_methods.R
+++ forward_methods.R
@@ -1,3 +1,5 @@
+#!/usr/bin/Rscript
+#  forward_methods.R Author "Nathan Wycoff <nathanbrwycoff@gmail.com>" Date 09.29.2017
 #################################################################################
 ##### Functions for forwards
 #An inner product function
@@ -52,4 +54,80 @@ forward_mds <- function(high_d, k, weights, dist.func, n.inits, seed) {
     z_optimal$par <- matrix(z_optimal$par, ncol = k)
 
     return(z_optimal)
+}
+
+#Creates the transform from the previous point
+make.B <- function(true_dist, low_d) {
+    #Create the low D distance matrix
+    low_dist <- as.matrix(dist(low_d))
+
+    n <- dim(true_dist)[1]
+    B <- matrix(0, ncol = n, nrow = n)
+    
+    #iterate over the B matrix to create it
+    for (i in 1:n) {
+        for (j in 1:n) {
+            if (i != j) {
+                B[i,j] <- -true_dist[i,j] / low_dist[i,j]
+            }
+        }
+    }
+
+    #Create the diagonal elements
+    diag(B) <- -colSums(B)
+
+    return(B)
+}
+
+smacof_forward_mds <- function(high_d, weights, dist.func = euclidean.dist,
+                   thresh = 1e-5, max.iters = 1000, n.inits = 10) {
+    #Center high D distances
+    high_d <- scale(high_d)
+    high_d <- high_d %*% diag(weights)
+
+    #Get our high D distance
+    true_dist <- good.dist(high_d, dist.func)
+
+    #Run a bunch of smacof algos
+    results <- lapply(1:n.inits, function(i)
+                    single_smacof(true_dist, dist.func = dist.func,
+                        thresh = thresh, max.iters = max.iters))
+
+    #Get lowest cost result
+    costs <- sapply(results, function(i) i$cost)
+
+    return(results[[which.min(costs)]])
+}
+
+single_smacof <- function(true_dist, dist.func = euclidean.dist,
+                   thresh = 1e-5, max.iters = 1000) {
+    #Make a random initial lowD matrix
+    low_d <- matrix(rnorm(n*2), ncol = 2)
+
+    ##Do a SMACOF iteration
+    diff <- Inf
+    last_err <- Inf
+    iter <- 0
+    while (diff > thresh && iter < max.iters) {
+        iter <- iter + 1
+
+        #Create the B matrix
+        B <- make.B(true_dist, low_d)
+
+        #Get our next Z
+        low_d <- 1/n * B %*% low_d
+
+        #Print current error
+        err <- forward_cost(low_d, true_dist, 2, dist.func)
+
+        #Get difference in error
+        diff <- abs(last_err - err)
+        last_err <- err
+    }
+
+    if (iter == max.iters) {
+        print("Warning: Did not converge")
+    }
+
+    return(list(low_d = low_d, cost = err))
 }
--- inverse_test.R
+++ inverse_test.R
@@ -10,14 +10,12 @@ n.inits <- 1
 dist.func <- inner.prod.dist
 seed <- 123
 
-
 #Generate the weights and induced lowD points
 high_d <- matrix(rnorm(n*p), ncol = p)
 user_weights <- rgamma(p, 1, 1)
 user_weights <- user_weights / sum(user_weights)
 low_d_result <- forward_mds(high_d, k, user_weights, dist.func, n.inits, seed + 1)
 user_low_d <- low_d_result$par
-
 
 #Try to recover those weights based on the low D points
 forward.n.inits <- n.inits#What we used to call n.inits changes
