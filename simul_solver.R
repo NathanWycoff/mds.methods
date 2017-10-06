ba54e3effd6e9ff4064ba63b4c04c170caa021e1
Nathan Wycoff <nathanbrwycoff@gmail.com>
2017-10-06 19:41:30 -0400

    SimulSolver can get rough answers

--- simul_solver.R
+++ simul_solver.R
@@ -24,6 +24,7 @@ make.B <- function(true_dist, low_d) {
     return(B)
 }
 
+##Get the cost for the approximate SMACOF-based inverse MDS algorithm
 approx_smacof_inverse_cost <- function(weights, user_low_d_dist, 
                                  high_d, prev_low_d, n.inits, dist.func) {
     #Get the low d projection induced by the weights, and its distance matrix
@@ -45,20 +46,58 @@ approx_smacof_inverse_cost <- function(weights, user_low_d_dist,
     return(stress)
 }
 
-approx_smacof_inverse_step <- function(user_low_d, prev_low_d, high_d, prev_weights, dist.func, forward.n.inits, seed) {
+#Opmitize the weights for 1 step of the approximate SMACOF algo
+approx_smacof_inverse_step <- function(user_low_d, prev_low_d, high_d, 
+                                       prev_weights, dist.func, forward.n.inits, 
+                                       seed) {
     set.seed(seed)
 
     #Calculate the low d distance matrix
     user_low_d_dist <- good.dist(user_low_d, dist.func)
 
-    #Get some random
     p <- dim(high_d)[2]
 
     #Run the solver a couple times
     result <- optim(prev_weights, approx_smacof_inverse_cost, method = "L-BFGS-B",
                     user_low_d_dist = user_low_d_dist, high_d = high_d, 
                     prev_low_d = prev_low_d, n.inits = forward.n.inits, 
-                    dist.func = dist.func, lower = 0.01)
+                    dist.func = dist.func, lower = 0.0001)
 
     return(result)
+}
+
+approx_smacof_simul <- function(user_low_d, prev_low_d, high_d, 
+                               prev_weights, dist.func, forward.n.inits, thresh,
+                               max.iters, seed) {
+
+    #Initialize on the last values
+    weights <- prev_weights
+    Z <- prev_low_d
+
+    #Iterate between optimizing weights and low_d coords
+    diff <- Inf
+    last_err <- Inf
+    iter <- 0
+    while (diff > thresh && iter < max.iters) {
+        in_seed <- sample(1:10000, 1)
+
+        #Update weights
+        last_weights <- weights
+        weights <- approx_smacof_inverse_step(user_low_d, Z, high_d, weights,
+                                              dist.func, forward.n.inits,
+                                              in_seed)$par
+        weights <- weights / sum(weights)
+        
+        #Update low D points
+        w_high_d <- high_d %*% diag(sqrt(weights))
+        true_dist <- good.dist(w_high_d, dist.func)
+        Z <- single_smacof(true_dist, dist.func = dist.func, thresh = thresh, 
+                           max.iters = max.iters)$par
+
+        #Update iterative params
+        diff <- norm(weights - last_weights, '2')
+        iter <- iter + 1
+    }
+
+    return(list(weights = weights, Z = Z))
 }
--- simul_tester.R
+++ simul_tester.R
@@ -6,8 +6,11 @@ source('simul_solver.R')
 
 ##Generate some weights and try to recover them.
 #Some params
+thresh <- 1e-5
+max.iters <- 1000
+
 #Create some data
-seed <- 1234
+seed <- 123
 set.seed(seed)
 p <- 5
 n <- 5
@@ -31,7 +34,8 @@ low_d_result <- forward_mds(high_d, k, init_weights, dist.func, n.inits, seed + 
 init_low_d <- low_d_result$par
 
 #Perturb the weights a little bit, get the projection (this represents after user interaction)
-perturb_weights <- init_weights * rgamma(p,50,50)
+perturb_weights <- rep(1,p)#rgamma(p,1,1)#init_weights * rgamma(p,100,10)
+perturb_weights[2] <- 10
 perturb_weights <- perturb_weights / sum(perturb_weights)
 low_d_result <- forward_mds(high_d, k, perturb_weights, dist.func, n.inits, seed + 1)
 perturb_low_d <- low_d_result$par
@@ -55,19 +59,68 @@ print(paste("Approx SMACOF cost with no perturb:", a))
 print(paste("Exact SMACOF cost with no perturb:", b))
 
 ################################################
-##If we start with the correct weights and only perturb a little bit, can we recover the new weights?
-forward.n.inits <- 10
-system.time(optim_result <- approx_smacof_inverse_step(perturb_low_d, init_low_d, 
-                                          high_d, init_weights, dist.func, 
-                                          forward.n.inits, seed + 1))
-infer_weights <- optim_result$par / sum(optim_result$par)
+##If we start with exactly the correct weights, how far do we move?
+#forward.n.inits <- 10
+#system.time(optim_result <- approx_smacof_inverse_step(init_low_d, init_low_d, 
+#                                          high_d, init_weights, dist.func, 
+#                                          forward.n.inits, seed + 1))
+#infer_weights <- optim_result$par / sum(optim_result$par)
+#
+#print("Init Weights:")
+#print(init_weights)
+#print("Perturb Weights:")
+#print(perturb_weights)
+#print("My guess:")
+#print(infer_weights)
+#print(paste('mine',sum(abs(infer_weights - perturb_weights))))
+#print(paste('init',sum(abs(init_weights - perturb_weights))))
+#print(paste('unif',sum(abs(mean(perturb_weights) - perturb_weights))))
+
+
+################################################
+###If we start with the correct weights and only perturb a little bit, can we recover the new weights using only 1 inverse step?
+#forward.n.inits <- 10
+#system.time(optim_result <- approx_smacof_inverse_step(perturb_low_d, init_low_d, 
+#                                          high_d, init_weights, dist.func, 
+#                                          forward.n.inits, seed + 1))
+#infer_weights <- optim_result$par / sum(optim_result$par)
+#
+#print("Init Weights:")
+#print(init_weights)
+#print("Perturb Weights:")
+#print(perturb_weights)
+#print("My guess:")
+#print(infer_weights)
+#print(paste('mine',sum(abs(infer_weights - perturb_weights))))
+#print(paste('init',sum(abs(init_weights - perturb_weights))))
+#print(paste('unif',sum(abs(mean(perturb_weights) - perturb_weights))))
+
+#################################################
+## Run the entire inverse algo, trying to recover the weights
+system.time(result <- approx_smacof_simul(perturb_low_d, init_low_d, high_d, 
+                               init_weights, dist.func, forward.n.inits, thresh, 
+                               max.iters, seed))
+infer_weights <- result$weights
 
 print("Init Weights:")
 print(init_weights)
+
 print("Perturb Weights:")
 print(perturb_weights)
+
 print("My guess:")
 print(infer_weights)
+
 print(paste('mine',sum(abs(infer_weights - perturb_weights))))
 print(paste('init',sum(abs(init_weights - perturb_weights))))
 print(paste('unif',sum(abs(mean(perturb_weights) - perturb_weights))))
+
+
+##Get the SMACOF cost of the two solutions
+#p_dist <- good.dist(perturb_low_d, dist.func)
+#approx_smacof_inverse_cost(infer_weights, p_dist, 
+#                                 high_d, init_low_d, n.inits, dist.func)
+#approx_smacof_inverse_cost(init_weights, p_dist, 
+#                                 high_d, init_low_d, n.inits, dist.func)
+#approx_smacof_inverse_cost(perturb_weights, p_dist, 
+#                                 high_d, init_low_d, n.inits, dist.func)
